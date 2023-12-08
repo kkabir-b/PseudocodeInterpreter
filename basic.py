9f8d16c1485c36365e0f3067b3b23d0071c41f1b
kkabir-b <107753204+kkabir-b@users.noreply.github.com>
2023-12-08 18:14:54 +0800

    Update basic.py
    added interpreter, logic for basic arithmetic and division by 0 error(as well as traceback context to be used later)

--- basic.py
+++ basic.py
@@ -31,8 +31,32 @@ class IllegalCharError(Error):
 
 
 class InvalidSyntaxError(Error):
-    def __init__(self, pos_start, pos_end, details):
+    def __init__(self, pos_start, pos_end, details = ''):
         super().__init__(pos_start, pos_end, 'Invalid syntax', details)
+
+class RTError(Error):
+    def __init__(self, pos_start, pos_end, details,context):
+        super().__init__(pos_start, pos_end, 'Runtime error', details)
+        self.context = context
+
+    def as_string(self):
+        result = self.generate_traceback()
+        result += f'{self.error_name}:{self.details}\n'
+        result += '\n\n' + string_with_arrows(self.pos_start.ftxt, self.pos_start, self.pos_end) + '\n'
+        return result
+
+    def generate_traceback(self):
+        result = ''
+        pos = self.pos_start
+        ctx = self.context
+
+        while ctx:
+            result = f'   File {pos.fn}, line {str(pos.ln + 1)}, in {ctx.display_name}\n' + result
+            pos = ctx.parent_entry_pos
+            ctx = ctx.parent
+
+        return 'Traceback (most recent call last ):\n' + result
+
 #Position
 #-------------------------------------------
 
@@ -163,6 +187,8 @@ class Lexer:
 class NumberNode:
     def __init__(self,tok):
         self.tok = tok
+        self.pos_start = self.tok.pos_start
+        self.pos_end = self.tok.pos_end
 
     def __repr__(self):
         return f'{self.tok}'
@@ -173,6 +199,8 @@ class BinOpNode:
         self.op_tok = op_tok
         self.right_node = right_node
 
+        self.pos_start = self.left_node.pos_start
+        self.pos_end = self.right_node.pos_end
 
     def __repr__(self):
         return f'({self.left_node},{self.op_tok},{self.right_node})'
@@ -182,6 +210,9 @@ class UnaryOpNode:
     def __init__(self,op_tok,node):
         self.op_tok = op_tok
         self.node = node
+
+        self.pos_start = self.op_tok.pos_start
+        self.pos_end = self.op_tok.pos_end
 
     def __repr__(self):
         return f'({self.op_tok}, {self.node})'
@@ -285,6 +316,136 @@ class Parser:
             left = BinOpNode(left, op_tok, right)
 
         return res.success(left)
+
+#runtime result
+#-------------------------------------------------
+
+class RTResult:
+    def __init__(self):
+        self.value = None
+        self.error = None
+
+    def register(self,res):
+        if res.error: self.error = res.error
+        return  res.value
+
+    def success(self,value):
+        self.value = value
+
+        return self
+
+    def failure(self,error):
+        self.error = error
+
+        return self
+
+#values
+#-------------------------------------------------
+
+class Number:
+
+    def __init__(self,value):
+        self.value = value
+        self.set_context()
+        self.set_pos()
+    def set_context(self,context = None):
+        self.context = context
+        return self
+
+    def set_pos(self,pos_start = None,pos_end = None):
+        self.pos_start = pos_start
+        self.pos_end = pos_end
+        return self
+
+    def added_to(self,other):
+        if isinstance(other,Number):
+            return Number(self.value + other.value).set_context(self.context), None
+
+    def subtracted_by(self,other):
+        if isinstance(other,Number):
+            return Number(self.value - other.value).set_context(self.context), None
+
+    def multiplied_by(self,other):
+        if isinstance(other,Number):
+            return Number(self.value * other.value).set_context(self.context), None
+
+    def divided_by(self,other):
+        if isinstance(other,Number):
+            if other.value == 0:
+                return None,RTError(
+                    other.pos_start
+                    ,other.pos_end,
+                    'Divison by Zero',
+                    self.context
+                )
+            return Number(self.value / other.value).set_context(self.context), None
+
+    def __repr__(self):
+        return f'{self.value}'
+
+#context
+#--------------------------------------------------
+
+class Context:
+    def __init__(self,display_name, parent = None, parent_entry_pos = None):
+        self.display_name = display_name
+        self.parent = parent
+        self.parent_entry_pos = parent_entry_pos
+
+#interpreter
+#--------------------------------------------------
+
+class Interpreter:
+    def visit(self,node,context):
+        method_name = f'visit_{type(node).__name__}'
+        method = getattr(self,method_name,self.no_visit_method)
+        return method(node,context)
+
+
+    def no_visit_method(self,node,context):
+        raise Exception(f'No visit_{type(node).__name__} defined')
+
+    def visit_NumberNode(self,node,context):
+        return RTResult().success(
+            Number(node.tok.value).set_context(context).set_pos(node.pos_start,node.pos_end)
+        )
+
+    def visit_BinOpNode(self,node,context):
+        res = RTResult()
+        left = res.register(self.visit(node.left_node,context))
+        if res.error: return res
+        right = res.register(self.visit(node.right_node,context))
+        if res.error: return res
+
+        if node.op_tok.type == TT_PLUS:
+            result,error = left.added_to(right)
+
+        if node.op_tok.type == TT_MINUS:
+            result,error = left.subtracted_by(right)
+
+        if node.op_tok.type == TT_MUL:
+            result,error = left.multiplied_by(right)
+
+        if node.op_tok.type == TT_DIV:
+            result,error = left.divided_by(right)
+
+
+        if error: return res.failure(error)
+        return res.success(result.set_pos(node.pos_start,node.pos_end))
+
+    def visit_UnaryOpNode(self,node,context):
+        res = RTResult()
+        number = res.register(self.visit(node.node,context))
+        if res.error: return res
+
+        error = None
+
+        if node.op_tok.type == TT_MINUS:
+            number,error = number.multiplied_by(Number(-1))
+
+        if error: return res.failure(error)
+        return res.success(number.set_pos(node.pos_start,node.pos_end))
+
 #run
 #--------------------------------------------------
 
@@ -298,4 +459,11 @@ def run(fn,text):
     #generate ast
     parser = Parser(tokens)
     ast = parser.parse()
-    return ast.node,ast.error
+    if ast.error: return None,ast.error
+
+    #runs
+    interpreter = Interpreter()
+    context = Context('<program>')
+    result = interpreter.visit(ast.node,context)
+
+    return result.value,result.error
