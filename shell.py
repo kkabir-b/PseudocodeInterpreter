d30edeca1d55b3b00ced292fbdce8d365ebc51c4
kkabir-b <107753204+kkabir-b@users.noreply.github.com>
2024-01-21 23:32:46 +0800

    Update shell.py
    Added ability to run files

--- shell.py
+++ shell.py
@@ -1,8 +1,14 @@
 import basic
-
-
-while True:
-    text = input('stdin >')
-    result,error = basic.run('Standard input',text)
-    if error: print(error.as_string())
-    elif result:print(result)
+# while True:
+#     text = input('stdin >')
+#     result,error = basic.run('Standard input',text)
+#     if error: print(error.as_string())
+#     elif result:
+#         if len(result.elements) == 1:
+#             print(repr(result.elements[0]))
+#         else:
+#             print(repr(result))
+fn = input('Enter file name->')
+f = open(fn+'.txt','r').read()
+_,error = basic.run(fn,f)
+if error:print(error.as_string())
