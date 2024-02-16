ef96a5e45f63f5da64e95d1e9780d0dff4a40310
kkabir-b <107753204+kkabir-b@users.noreply.github.com>
2024-02-16 13:21:15 +0800

    Update shell.py

--- shell.py
+++ shell.py
@@ -1,14 +1,11 @@
 import pseudocode
-# while True:
-#     text = input('stdin >')
-#     result,error = basic.run('Standard input',text)
-#     if error: print(error.as_string())
-#     elif result:
-#         if len(result.elements) == 1:
-#             print(repr(result.elements[0]))
-#         else:
-#             print(repr(result))
+#run function
+def runPseudocode(f):
+    f.replace('\n',';')
+    _,error = pseudocode.run(fn,f)
+    if error:print(error.as_string())
+
+#simple script to run a file
 fn = input('Enter file name->')
-f = open(fn+'.txt','r').read()
-_,error = pseudocode.run(fn,f)
-if error:print(error.as_string())
+file = open(fn+'.txt','r').read()
+runPseudocode(file)
